524cec3e1435ad05ef970b057671239822ac400d
Huy Do Duc <dod.huy@gmail.com>
2019-10-25 18:34:21 +0700

    support s3 upload

--- index.js
+++ index.js
@@ -6,10 +6,15 @@ module.exports = createReadStream;
 module.exports.createReadStream = createReadStream;
 
 function createReadStream (path, options) {
-  var ds = stream.Duplex({ objectMode: true });
+  var ds = new stream.Duplex({ objectMode: true });
   options.autoClose = false;
   var tail = options && !options.end && options.tail;
+  // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3/ManagedUpload.html#minPartSize-property
+  var partSize = (options && options.partSize) || (5 * 1024 * 1024);
   var bytesRead = 0;
+  var chunks = [];
+  var chunksLength = 0;
+  
   var close = function () {
   };
 
@@ -21,6 +26,7 @@ function createReadStream (path, options) {
     var chunkSize = 64 * 1024;
     var pos = 0;
     var watcher;
+    var interval;
     var reading = false;
     var watching = false;
     var synced = false;
@@ -31,18 +37,30 @@ function createReadStream (path, options) {
           watcher = fs.watch(path, function (eventType, fileName) {
             // only kick of a read if already hit the end of the file
             ds.emit('change');
+            console.log(new Date(), eventType, fileName)
             if (!reading && synced) {
+              console.log(new Date(), 'read last chunk')
               reading = true;
               readChunk();
             }
           });
+
+          interval = setInterval(() => {
+            if (!reading && synced) {
+              reading = true;
+              readChunk();
+            }
+          }, 1000)
+
           watching = true;
           close = function () {
             watching = false;
             watcher.close();
+            clearInterval(interval);
             if (!reading && synced) {
-              ds.push(null);
+              pushChunk(null);
             }
+            console.log(new Date(), 'close invoked', reading, synced)
           };
         }
       })
@@ -52,7 +70,8 @@ function createReadStream (path, options) {
           ds.push(null);
         }
         synced = true;
-        ds.emit('sync');
+        ds.emit('sync')
+        // console.log(new Date(), 'sync', bytesRead, pos)
       })
       .pipe(ds, { end: false });
 
@@ -60,13 +79,19 @@ function createReadStream (path, options) {
       var b = Buffer.alloc(chunkSize);
       fs.read(rs.fd, b, 0, chunkSize, pos, function (err, bytesRead) {
         if (err) return ds.emit('error', err);
+        // console.log(new Date(), pos, bytesRead)
         if (bytesRead) {
           pos += bytesRead;
-          var data = b.slice(0, bytesRead).toString(options.encoding);
-          ds.push(data);
+          var data = b.slice(0, bytesRead);
+          if (options.encoding) {
+            data = data.toString(options.encoding);
+          }
+
+          pushChunk(data)
           setImmediate(readChunk);
         } else if (reading) {
           reading = false;
+          ds.emit('eof')
           if (!watching) {
             ds.push(null);
           }
@@ -77,9 +102,28 @@ function createReadStream (path, options) {
 
   ds._write = function (data, enc, cb) {
     bytesRead += Buffer.byteLength(data);
-    ds.push(data);
+    // console.log(new Date(), 'write', data ? '' : data)
+    pushChunk(data)
     cb();
   };
+
+  function pushChunk(chunk) {
+    if (chunk) {
+      chunks.push(chunk);
+      chunksLength += chunk.length
+    }
+
+    if (chunksLength >= partSize || chunk === null) {
+      // console.log(new Date(), `Push ${chunksLength} bytes of data`)
+      ds.push(Buffer.concat(chunks));
+      chunks = [];
+      chunksLength = 0;
+
+      if (chunk === null) {
+        ds.push(null)
+      }
+    }
+  }
 
   return ds;
 }
